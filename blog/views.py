9586cd290325c6f74215c781681859934caf2174
svg83 <svgeneraloff@gmail.com>
2023-04-07 15:58:44 +0400

    optimazed commets count_2

--- blog/views.py
+++ blog/views.py
@@ -48,11 +48,22 @@ def serialize_tag(tag):
 
 def index(request):
  
+##    most_popular_posts = Post.objects.prefetch_related(
+##        'author', 'comments').annotate(
+##        comments_count = Count('comments',distinct=True),
+##        likes_count = Count('likes',distinct=True) 
+##        ).order_by('-likes_count')[:5]
+
     most_popular_posts = Post.objects.prefetch_related(
-        'author', 'comments').annotate(
-        comments_count = Count('comments',distinct=True),
-        likes_count = Count('likes',distinct=True) 
+        'author').annotate(likes_count = Count('likes') 
         ).order_by('-likes_count')[:5]
+    most_popular_posts_ids = [post.id for post in most_popular_posts]
+    posts_with_comments = Post.objects.filter(
+        id__in=most_popular_posts_ids).annotate(comments_count=Count('comments'))
+    ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
+    count_for_id = dict(ids_and_comments)
+    for post in most_popular_posts:
+        post.comments_count = count_for_id[post.id]
 
     fresh_posts = Post.objects.prefetch_related(
         'author').annotate(
