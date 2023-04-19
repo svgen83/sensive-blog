d5da330a61c42746b7d574917db2998f1638520f
svg83 <svgeneraloff@gmail.com>
2023-04-19 09:49:10 +0400

    used related name comments in Comments.post

--- blog/views.py
+++ blog/views.py
@@ -53,7 +53,7 @@ def index(request):
 def post_detail(request, slug):
     post = get_object_or_404(
         Post.objects.annotate(likes_count=Count('likes')), slug=slug)
-    comments = Comment.objects.filter(post=post).prefetch_related('author')
+    comments = post.comments.prefetch_related('author')
     serialized_comments = []
     for comment in comments:
         serialized_comments.append({
